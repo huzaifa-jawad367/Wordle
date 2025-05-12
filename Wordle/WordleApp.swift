355c80ca8f612dbddbde35c1a99b1647a3b8bdb6
huzaifa-jawad367 <hjawad.bscs21seecs@seecs.edu.pk>
2025-05-12 18:08:59 +0500

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Huzaifa Jawad on 12/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WordleTests/WordleTests.swift
@@ -0,0 +1,17 @@
+//
+//  WordleTests.swift
+//  WordleTests
+//
+//  Created by Huzaifa Jawad on 12/05/2025.
+//
+
+import Testing
+@testable import Wordle
+
+struct WordleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
